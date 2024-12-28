35ad6661785c8f8e5bc87dd0e30affe494faf0ac
giuliodalbono <dalbonogiulio@gmail.com>
2024-12-28 18:53:01 +0100

    Implemented Webhook and Githook for continuos deployment

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+import git
 
 app = Flask(__name__)
 
 
+@app.route('/git-update', methods=['POST'])
+def git_update():
+    repo = git.Repo("./macc-project-python")
+    origin = repo.remotes.origin
+    repo.create_head("main", origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return "", 200
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Githook works!'
 
 
 if __name__ == '__main__':
