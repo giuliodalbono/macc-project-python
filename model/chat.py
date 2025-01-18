05ae630489f2b383373ddf457fa4ab024bc8ae64
giuliodalbono <dalbonogiulio@gmail.com>
2025-01-18 11:33:14 +0100

    test creationTime

--- model/chat.py
+++ model/chat.py
@@ -4,6 +4,7 @@ from sqlalchemy import ForeignKey
 
 from db import db
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
 
 
 class Chat(db.db.Model):
@@ -11,8 +12,8 @@ class Chat(db.db.Model):
     name: Mapped[str] = mapped_column(nullable=False)
     is_public: Mapped[bool] = mapped_column(nullable=False, default=False)
     user_id: Mapped[str] = mapped_column(ForeignKey('user.uid'), nullable=False)
-    creation_time: Mapped[datetime] = mapped_column(nullable=False)
-    last_update: Mapped[datetime] = mapped_column(nullable=False)
+    creation_time: Mapped[datetime] = mapped_column(nullable=False, server_default=func.now())
+    last_update: Mapped[datetime] = mapped_column(nullable=False, server_default=func.now())
 
     def to_dict(self):
         return {"id": self.id, "name": self.name, "is_public": self.is_public, "user_id": self.user_id,
