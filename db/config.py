715577d631ae194b9f131211b2662fd62a1a9699
giuliodalbono <dalbonogiulio@gmail.com>
2025-01-18 11:48:20 +0100

    Revert "test db url connection"
    This reverts commit 2f4118ffd45ee2ed701e5cedb2d8ebbdc49d9ac7.

--- db/config.py
+++ db/config.py
@@ -3,5 +3,5 @@ class Config:
     MYSQL_USER = 'ghinoads'
     MYSQL_PASSWORD = '63#ytMzZ;y~sd+fDn#VX8KqK'
     MYSQL_DB = 'ghinoads$macc_project'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ghinoads:63#ytMzZ;y~sd+fDn#VX8KqK@ghinoads.mysql.pythonanywhere-services.com/ghinoads$macc_project?useLegacyDatetimeCode=false&serverTimezone=Europe/Rome'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ghinoads:63#ytMzZ;y~sd+fDn#VX8KqK@ghinoads.mysql.pythonanywhere-services.com/ghinoads$macc_project'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
