524f64f3dac98ccab70753ecb46e33d3b34f0818
giuliodalbono <dalbonogiulio@gmail.com>
2025-01-18 13:04:01 +0100

    added pool recycle option

--- db/config.py
+++ db/config.py
@@ -5,3 +5,4 @@ class Config:
     MYSQL_DB = 'ghinoads$macc_project'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ghinoads:63#ytMzZ;y~sd+fDn#VX8KqK@ghinoads.mysql.pythonanywhere-services.com/ghinoads$macc_project'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 280}
